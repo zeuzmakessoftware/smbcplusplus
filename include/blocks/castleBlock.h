abea71c042e521b149a18618e6fb4072823b01eb
zeuzmakessoftware <aodude@icloud.com>
2026-05-11 12:27:26 -0700

    feat: added static castle block for 1-4

---
+++ include/blocks/castleBlock.h
@@ -0,0 +1,25 @@
+#ifndef CASTLE_BLOCK_H
+#define CASTLE_BLOCK_H
+
+#include "blocks/base.h"
+
+class castleBlock : public Block {
+protected:
+    Rectangle source;
+    bool collidable;
+
+public:
+    castleBlock(int x, int y, Texture2D sprites, Rectangle source, bool collidable,
+                const SceneType& scene = GetSceneType(SceneKind::Overworld));
+    void update(Rectangle marioRec, float& marioVelY, bool isBig) override {}
+    void draw() override;
+    bool hasCollision() const override;
+};
+
+class castleBrickBlock : public castleBlock {
+public:
+    castleBrickBlock(int x, int y, Texture2D sprites,
+                            const SceneType& scene = GetSceneType(SceneKind::Overworld));
+};
+
+#endif
---
+++ src/blocks/castleBlock.cpp
@@ -0,0 +1,29 @@
+#include "blocks/castleBlock.h"
+
+namespace {
+const Rectangle castleBrickBlockSource = {0.0f, 151.0f, 16.0f, 16.0f};
+}
+
+castleBlock::castleBlock(int x, int y, Texture2D sprites, Rectangle source, bool collidable,
+                         const SceneType& scene)
+    : Block(x, y, sprites, scene), source(source), collidable(collidable) {}
+
+void castleBlock::draw() {
+    DrawTexturePro(
+        spriteSheet,
+        source,
+        (Rectangle){(float)rectXPos, (float)rectYPos, (float)TILE_SIZE, (float)TILE_SIZE},
+        (Vector2){0, 0},
+        0.0f,
+        WHITE
+    );
+}
+
+bool castleBlock::hasCollision() const {
+    return collidable;
+}
+
+castleBrickBlock::castleBrickBlock(int x, int y, Texture2D sprites, const SceneType& scene)
+    : castleBlock(x, y, sprites, castleBrickBlockSource, true, scene) {}
+
+REGISTER_LEVEL_EDITOR_BLOCK(castleBrickBlock, castleBrickBlockSource);
